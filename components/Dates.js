260723edc4ec6510822ee843417083dc1e19f73a
NirpendraChaudhary <nirpendra@spur.fit>
2021-05-26 12:23:01 +0530

    first commit

---
+++ components/Dates.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import moment from "moment";
+import DateRangePicker from "react-native-daterange-picker";
+
+export default class Dates extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      endDate: null,
+      displayedDate: moment(),
+    };
+  }
+
+  setDates = (dates) => {
+    this.setState({
+      ...dates,
+    });
+  };
+
+  render() {
+    console.log(this.props);
+    const { startDate, endDate, displayedDate } = this.state;
+    const { setStartDate, setEndDate } = this.props.route.params;
+    setStartDate(startDate);
+    setEndDate(endDate);
+
+    return (
+      <View style={styles.container}>
+        <DateRangePicker
+          onChange={this.setDates}
+          endDate={endDate}
+          startDate={startDate}
+          displayedDate={displayedDate}
+          range
+        >
+          <Text>Click me!</Text>
+        </DateRangePicker>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
